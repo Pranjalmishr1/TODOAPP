a03cf914f86427f6716c0ee4aa2d1af27678c0d6
Pranjalmishr1 <24pranjalmishra2005@gmail.com>
2024-12-09 14:25:45 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoappComponent } from './todoapp/todoapp.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoappComponent,
+    TictactoeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
